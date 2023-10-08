be7f8e9cb6c98d544d0ba95f3b71acf4194087ff
iliabes <oksana14888@gmail.com>
2023-10-08 18:03:15 +0500

    adaptive nav menu

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 
 import './sass/main.sass'
 
+let burger = document.getElementById('burger')
+let headerRow = document.getElementById('headerRow')
+
+burger.addEventListener('click',()=>{
+    burger.classList.toggle('active')
+    headerRow.classList.toggle('header__top-row-mobile')
+})
+
+
